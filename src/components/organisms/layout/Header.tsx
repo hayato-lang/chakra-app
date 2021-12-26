c63e216a10bd6c7c812c0c02ee39c6787e594b4b
hayato-lang <j.h.14.s.g.8@gmail.com>
2021-12-26 18:43:39 +0900

    ヘッダーの作成２

--- src/components/organisms/layout/Header.tsx
+++ src/components/organisms/layout/Header.tsx
@@ -1,9 +1,23 @@
-import { Box, Flex, Heading, IconButton, Link } from "@chakra-ui/react";
+import {
+   Box, 
+   Button, 
+   Drawer, 
+   DrawerBody, 
+   DrawerOverlay, 
+   Flex, 
+   Heading, 
+   IconButton, 
+   Link, 
+   useDisclosure 
+} from "@chakra-ui/react";
 import { memo, VFC } from "react";
 import { HamburgerIcon } from "@chakra-ui/icons";
 
+ 
 export const Header: VFC = memo(() => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   return (
+    <>
       <Flex
         as="nav"
         bg="teal.500"
@@ -12,16 +26,21 @@ export const Header: VFC = memo(() => {
         justify="space-between"
         padding={{ base: 3, md: 5 }}
       >
-        <Flex align="center" as="a" mr={8} _hover={{ cursor: "pointer" }}>
+        <Flex 
+          align="center" 
+          as="a" 
+          mr={8} 
+          _hover={{ cursor: "pointer" }}
+        >
           <Heading as="h1" fontSize={{ base:"md", md:"lg" }}>
             ユーザー管理アプリ
           </Heading>
         </Flex>
         <Flex
-        align="center"
-        fontSize="sm"
-        flexGrow={2}
-        display={{ base: "none", md: "flex" }}
+          align="center"
+          fontSize="sm"
+          flexGrow={2}
+          display={{ base: "none", md: "flex" }}
         >
           <Box pr={4}>
             <Link>ユーザー一覧</Link>
@@ -29,12 +48,23 @@ export const Header: VFC = memo(() => {
             <Link>設定</Link>
         </Flex>
         <IconButton 
-          aria-label="メニューボタン" 
-          icon={<HamburgerIcon/>}
+          aria-label="メニューボタン"
+          icon={<HamburgerIcon />}
           size="sm" 
           variant="unstyled" 
           display={{ base: "block", md: "none" }}
+          onClick={onOpen}
         />
       </Flex>
+      <Drawer placement="left" size="xs" isOpen={isOpen} onClose={onClose}>
+        <DrawerOverlay>
+          <DrawerBody p={0} bg="gray.100">
+            <Button w="100%">TOP</Button>
+            <Button w="100%">ユーザー一覧</Button>
+            <Button w="100%">設定</Button>
+          </DrawerBody>
+        </DrawerOverlay>
+      </Drawer>
+    </>
   );
 });
