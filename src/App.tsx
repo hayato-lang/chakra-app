89d8190cf4b256de47bc077a0d69c0fd731c6d8a
hayato-lang <j.h.14.s.g.8@gmail.com>
2021-12-26 10:18:18 +0900

    ルーティング

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,17 @@ import * as React from "react"
 import {
   ChakraProvider
 } from "@chakra-ui/react"
+
+import { BrowserRouter } from "react-router-dom"
+
 import theme from "./theme/theme"
+import { Router } from "./router/Router"
 
 
 export const App = () => (
   <ChakraProvider theme={theme}>
-    
+     <BrowserRouter>
+      <Router/>
+     </BrowserRouter>
   </ChakraProvider>
 )
