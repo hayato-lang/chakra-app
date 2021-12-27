356fdedd428af44d41aa7157f1d93e45ad9b7151
hayato-lang <j.h.14.s.g.8@gmail.com>
2021-12-27 14:03:48 +0900

    ログイン機能の実装

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,8 +1,11 @@
 import { Box, Divider, Flex, Heading, Input, Stack } from "@chakra-ui/react";
-import { memo, VFC } from "react";
+import { ChangeEvent, memo, useState, VFC } from "react";
 import { PrimaryButton } from "../atoms/button/PrimaryButton";
 
 export const Login: VFC = memo(() => {
+  const [userId, setUserId] = useState('');
+
+  const onChangeUser = (e: ChangeEvent<HTMLInputElement>) => setUserId(e.target.value);
   return (
     <Flex align="center" justify="center" height="100vh">
       <Box bg="white" w="sm" p={4} borderRadius="md" shadow="md">
@@ -11,7 +14,7 @@ export const Login: VFC = memo(() => {
         </Heading>
           <Divider my={4} />
           <Stack spacing={6} py={4} px={10}>
-            <Input placeholder="ユーザーID"/>
+            <Input placeholder="ユーザーID" value={userId} onChange={onChangeUser}/>
             <PrimaryButton>ログイン</PrimaryButton>
           </Stack>
       </Box>
