cf798611afd406b99b5f0f6a1755e452872e3553
hayato-lang <j.h.14.s.g.8@gmail.com>
2021-12-28 09:06:39 +0900

    メッセージ表示機能

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -2,23 +2,26 @@ import axios from "axios";
 import { useCallback, useState } from "react";
 import { useHistory } from "react-router-dom";
 import { User } from "../types/api/user";
+import { useMessage } from "./useMessage";
 
 export const useAuth = () => {
   const history = useHistory();
 
+  const { showMessages } = useMessage();
   const [loading, setLoading] = useState(false);
   const login = useCallback((id: string) => {
     setLoading(true);
     axios.get<User>(`https://jsonplaceholder.typicode.com/users/${id}`)
     .then((res) => {
       if (res.data) {
+        showMessages({title: "ログインしました", status: "success"})
         history.push("/home")
       } else {
-        alert("ユーザーが見つかりません");
+        showMessages({title: "ユーザーが見つかりません", status: "error"})
       }
     })
-    .catch(() => alert("ログインできません"))
+    .catch(() => showMessages({title: "ユーザーが見つかりません", status: "error"}))
     .finally(() => setLoading(false))
-  }, [history]);
+  }, [history, showMessages]);
   return { login, loading }
 }
